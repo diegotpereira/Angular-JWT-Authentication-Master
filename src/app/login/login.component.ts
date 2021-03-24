97802ae7cefbba75f2071cc1e728d6b26a879e14
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-24 15:05:08 -0300

    alteração nos arquivos componentes;

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,55 +6,57 @@ import { first } from 'rxjs/operators';
 import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html'
+
+  templateUrl: 'login.component.html'
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  error = '';
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    error = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private authenticationService: AuthenticationService
-  ) {
+    constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+      private authenticationService: AuthenticationService
+    ) {
 
-    if (this.authenticationService.currentUserValue) {
-      this.router.navigate(['/']);
-    }
-  }
-
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
-  get f() { return this.loginForm.controls; }
-
-  onSubmit(){
-    this.submitted = true;
-
-    if (this.loginForm.invalid) {
-      return;
+      if (this.authenticationService.currentUserValue) {
+        this.router.navigate(['/']);
+      }
     }
 
-    this.loading = true;
-    this.authenticationService.login(this.f.username.value, this.f.password.value)
-        .pipe(first())
-        .subscribe({
-          next: () => {
-            const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-          },
-          error: error => {
-            this.error = error;
-            this.loading = false;
-          }
-        });
+    ngOnInit() {
+      this.loginForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+      });
+    }
+
+    get f() { return this.loginForm.controls; }
+
+    onSubmit(){
+      this.submitted = true;
+
+      if (this.loginForm.invalid) {
+        return;
+      }
+
+      this.loading = true;
+      this.authenticationService.login(this.f.username.value, this.f.password.value)
+          .pipe(first())
+          .subscribe({
+            next: () => {
+              const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+              this.router.navigate([returnUrl]);
+            },
+            error: error => {
+              this.error = error;
+              this.loading = false;
+            }
+          });
   }
 
 }
